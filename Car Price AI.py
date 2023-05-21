cfc413eb33190508349dd409ff6d1f366568de20
DennisKeenan <dennis.k.arkananta@gmail.com>
2023-05-21 11:41:14 +0700

    Used Car AI V2

--- Car Price AI.py
+++ Car Price AI.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plot
 import seaborn as sb
 data=pd.read_csv("Used_Car.csv")
 
+
 # # Data count
 # print(data.shape) 
 # print(data["year"].unique())
@@ -12,6 +13,7 @@ data=pd.read_csv("Used_Car.csv")
 # print(data["Price"].unique())
 # print(data["kms_driven"].unique())
 # print(data["fuel_type"].unique())
+
 
 # Data Duplicate and Fixation
 car=data.copy()
@@ -22,6 +24,7 @@ car["year"]=car["year"].astype(int)
 car=car[car["Price"]!="Ask For Price"]
 car["Price"]=car["Price"].str.replace(",","")
 car["Price"]=car["Price"].astype(int)
+car["Price"]=car["Price"]*179.91
     # Range Fixation
 car["kms_driven"]=car["kms_driven"].str.replace(",","")
 car["kms_driven"]=car["kms_driven"].str.replace(" kms","")
@@ -31,8 +34,30 @@ car["kms_driven"]=car["kms_driven"].astype(int)
 car=car[~ car["fuel_type"].isna()]
 print(car["fuel_type"].isna().sum())
 car["fuel_type"]=car["fuel_type"].astype(str)
-
+    # Car Name Slciing 
+car["name"]=car["name"].str.split(" ").str.slice(0,3).str.join(" ")
+    # Car Data Fixation
+car=car[car["Price"]<1000000000].reset_index(drop=True)
+car=car[car["kms_driven"]<300000].reset_index(drop=True)
+car=car[car["fuel_type"]!="LPG"].reset_index(drop=True)
+car=car[~((car["name"]=="Maruti Suzuki Zen") & (car["kms_driven"]==0))].reset_index(drop=True)
 print(car.info())
-print(car["fuel_type"].unique())
 
 
+# Relations
+    # Relation between company and prices
+plot.subplots(figsize=(15,7))
+ax=sb.boxplot(x="company",y="Price",data=car)
+ax.set_xticklabels(ax.get_xticklabels(),rotation=40,ha="right")
+plot.ticklabel_format(style="plain",axis="y")
+    # Relation between year and prices 
+plot.subplots(figsize=(15,7))
+ax=sb.boxplot(x="year",y="Price",data=car)
+ax.set_xticklabels(ax.get_xticklabels(),rotation=40,ha="right")
+plot.ticklabel_format(style="plain",axis="y")
+    # Relation between range and prices 
+plot.subplots(figsize=(15,7))
+ax=sb.relplot(x="kms_driven",y="Price",data=car)
+    # Relation between fuel type and prices
+
+plot.show()
